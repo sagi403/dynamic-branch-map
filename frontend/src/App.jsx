467693828eabde7d392896b997e241927970ac85
Sagi <sagi403@gmail.com>
2023-05-04 14:45:21 +0300

    Frontend project initial setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import MyModal from "./components/TestDialog.jsx";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-slate-500 text-cyan-500">TEST</h1>
+      <MyModal />
+    </>
+  );
+}
+
+export default App;
