341f8ae8e97579e5925dae84245b4ea71f652d5c
Sagi <sagi403@gmail.com>
2023-05-06 14:15:26 +0300

    Showing the suggestion list only on focus

--- frontend/src/screens/Homepage.jsx
+++ frontend/src/screens/Homepage.jsx
@@ -1,11 +1,12 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
 const Homepage = () => {
   const [location, setLocation] = useState("");
   const [selectedPlace, setSelectedPlace] = useState("");
   const [suggestions, setSuggestions] = useState([]);
   const [highlightedIndex, setHighlightedIndex] = useState(-1);
-  const inputRef = useRef(null);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+  const [containerMouseDown, setContainerMouseDown] = useState(false);
 
   useEffect(() => {
     if (!window.google) {
@@ -62,34 +63,40 @@ const Homepage = () => {
       <h1 className="text-4xl font-bold mb-8">The WWW Corp</h1>
       <h2 className="text-2xl mb-4">Search for branch</h2>
       <div className="w-full max-w-md">
-        <input
-          ref={inputRef}
-          type="text"
-          placeholder="Enter Address or Place"
-          className="w-full px-4 py-2 border border-gray-300 rounded shadow-sm focus:outline-none focus:border-cyan-500"
-          value={location}
-          onChange={e => setLocation(e.target.value)}
-          onKeyDown={handleKeyDown}
-        />
-        {suggestions.length > 0 && (
-          <div className="relative">
-            <div className="absolute z-10 w-full mt-1 bg-white border border-gray-300 rounded shadow-sm">
-              {suggestions.map((suggestion, index) => (
-                <div
-                  key={suggestion.place_id}
-                  className={`px-4 py-2 cursor-pointer ${
-                    index === highlightedIndex ? "bg-gray-200" : ""
-                  }`}
-                  onMouseEnter={() => setHighlightedIndex(index)}
-                  onMouseLeave={() => setHighlightedIndex(-1)}
-                  onClick={() => handleSuggestionClick(suggestion)}
-                >
-                  {suggestion.description}
-                </div>
-              ))}
+        <div
+          onMouseDown={() => setContainerMouseDown(true)}
+          onMouseUp={() => setContainerMouseDown(false)}
+        >
+          <input
+            type="text"
+            placeholder="Enter Address or Place"
+            className="w-full px-4 py-2 border border-gray-300 rounded shadow-sm focus:outline-none focus:border-cyan-500"
+            value={location}
+            onChange={e => setLocation(e.target.value)}
+            onKeyDown={handleKeyDown}
+            onBlur={() => !containerMouseDown && setShowSuggestions(false)}
+            onFocus={() => setShowSuggestions(true)}
+          />
+          {suggestions.length > 0 && showSuggestions && (
+            <div className="relative">
+              <div className="absolute z-10 w-full mt-1 bg-white border border-gray-300 rounded shadow-sm">
+                {suggestions.map((suggestion, index) => (
+                  <div
+                    key={suggestion.place_id}
+                    className={`px-4 py-2 cursor-pointer ${
+                      index === highlightedIndex ? "bg-gray-200" : ""
+                    }`}
+                    onMouseEnter={() => setHighlightedIndex(index)}
+                    onMouseLeave={() => setHighlightedIndex(-1)}
+                    onClick={() => handleSuggestionClick(suggestion)}
+                  >
+                    {suggestion.description}
+                  </div>
+                ))}
+              </div>
             </div>
-          </div>
-        )}
+          )}
+        </div>
         <div className="flex justify-center mt-4">
           <button
             className={`px-10 py-2 bg-cyan-500 text-white font-bold rounded focus:outline-none ${
