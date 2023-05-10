0b04063ce69ad1906fc71722c87ed2b2715d6f3b
Sagi <sagi403@gmail.com>
2023-05-10 21:36:22 +0300

    Adding server to serve files to the frontend

---
+++ frontend/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "dist", "index.html"));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
